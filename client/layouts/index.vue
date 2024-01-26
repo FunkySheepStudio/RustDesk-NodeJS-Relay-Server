65d449161fb03dec4b2751e8ee1af09bb62dbc82
Sébastien JARDE <s.jarde@auvernge-rhone-alpes.cci.fr>
2024-01-26 10:04:31 +0100

    Initial commit

---
+++ client/layouts/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+      <v-main>
+        <router-view />
+      </v-main>
+    </v-app>
+</template>
+<script>
+module.exports = {
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  mounted () {
+  },
+  computed: {
+  }
+}
+</script>
+<style>
+html {
+  overflow-y: scroll
+}
+
+/* Hiding scrollbar for Chrome, Safari and Opera */
+html::-webkit-scrollbar {
+    display: none;
+}
+
+/* Hiding scrollbar for IE, Edge and Firefox */
+html {
+  scrollbar-width: none;  /* Firefox */
+  -ms-overflow-style: none;  /* IE and Edge */
+}
+
+</style>
---
+++ client/pages/welcome.vue
@@ -0,0 +1,19 @@
+<template>
+    Welcome
+</template>
+  <script>
+    module.exports = {
+      components: {
+      },
+      data() {
+        return {
+        }
+      },
+      mounted() {
+      },
+      methods: {
+      }
+    }
+  </script>
+  <style>
+  </style>
